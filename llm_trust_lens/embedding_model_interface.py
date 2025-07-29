85e77d014f9f9364d83269d9098f6fe5b6b10244
KaiquanMah <7814298+KaiquanMah@users.noreply.github.com>
2025-07-29 16:19:15 +0800

    Delete llm_trust_lens directory

--- llm_trust_lens/embedding_model_interface.py
+++
@@ -1,8 +0,0 @@
-class EmbeddingModelInterface:
-    def get_embedding(self, text: str) -> list[float]:
-        """Returns an embedding vector for the input text."""
-        raise NotImplementedError
-
-    def retrieve_similar(self, query: str, top_k: int = 5) -> list[str]:
-        """Returns top-k retrieved documents for a given query."""
-        raise NotImplementedError
